b6494d889c2ea6029bb6121c994c66e5d85ac067
Fry-Fr <jwalker.ry@gmail.com>
2026-02-12 15:57:34 -0500

    update start server and connect with ampq

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	ampqURL := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(ampqURL)
+	if err != nil {
+		fmt.Printf("Failed to connect to RabbitMQ: %s\n", err)
+		return
+	}
+	defer connection.Close()
+
+	fmt.Println("Successfully connected to RabbitMQ")
 	fmt.Println("Starting Peril server...")
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	<-sigChan
+
+	fmt.Println("\nShutting down gracefully...")
+
 }

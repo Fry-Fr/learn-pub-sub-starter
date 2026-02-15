68d4cc60ea48dfd4ff804ce7aa5e5311d115733f
Fry-Fr <jwalker.ry@gmail.com>
2026-02-14 21:59:47 -0500

    update repl to send cmds

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -22,25 +21,54 @@ func main() {
 	fmt.Println("Successfully connected to RabbitMQ")
 	fmt.Println("Starting Peril server...")
 
-	channel, err := connection.Channel()
-	if err != nil {
-		fmt.Printf("Failed to open a channel: %s\n", err)
-		return
+	gamelogic.PrintServerHelp()
+	for {
+		inputSlice := gamelogic.GetInput()
+		if len(inputSlice) == 0 {
+			continue
+		}
+		command := inputSlice[0]
+		switch command {
+		case "pause":
+			fmt.Println("sending pause message...")
+			channel, err := connection.Channel()
+			if err != nil {
+				fmt.Printf("Failed to open a channel: %s\n", err)
+				return
+			}
+			defer channel.Close()
+
+			err = pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+				IsPaused: true,
+			})
+			if err != nil {
+				fmt.Printf("Failed to publish pause message: %s\n", err)
+				return
+			}
+
+		case "resume":
+			fmt.Println("sending resume message...")
+			channel, err := connection.Channel()
+			if err != nil {
+				fmt.Printf("Failed to open a channel: %s\n", err)
+				return
+			}
+			defer channel.Close()
+
+			err = pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+				IsPaused: false,
+			})
+			if err != nil {
+				fmt.Printf("Failed to publish resume message: %s\n", err)
+				return
+			}
+
+		case "quit":
+			fmt.Println("Quitting the server...")
+			return
+		default:
+			fmt.Printf("Unknown command: %s\n", command)
+			continue
+		}
 	}
-	defer channel.Close()
-
-	err = pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
-		IsPaused: true,
-	})
-	if err != nil {
-		fmt.Printf("Failed to publish pause message: %s\n", err)
-		return
-	}
-
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, os.Interrupt)
-	<-sigChan
-
-	fmt.Println("\nShutting down gracefully...")
-
 }

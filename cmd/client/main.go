0bcd501876dbf696ef06dc831945f20e79024375
Fry-Fr <jwalker.ry@gmail.com>
2026-02-14 21:01:03 -0500

    add logic to create que and bind exchange

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril client...")
+	ampqURL := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(ampqURL)
+	if err != nil {
+		fmt.Printf("Failed to connect to Peril client: %s\n", err)
+		return
+	}
+	defer connection.Close()
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		fmt.Printf("Failed to welcome client: %s\n", err)
+		return
+	}
+	exchange := routing.ExchangePerilDirect
+	routingKey := routing.PauseKey
+	queueName := fmt.Sprintf("%s.%s", routingKey, username)
+	_, _, err = pubsub.DeclareAndBind(connection, exchange, queueName, routingKey, pubsub.TransientQueue)
+	if err != nil {
+		fmt.Printf("Failed to declare and bind queue: %s\n", err)
+		return
+	}
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	<-sigChan
+
+	fmt.Println("\nShutting down gracefully...")
 }
---
+++ internal/pubsub/declareandbind.go
@@ -0,0 +1,39 @@
+package pubsub
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type SimpleQueueType string
+
+const (
+	DurableQueue   SimpleQueueType = "durable"
+	TransientQueue SimpleQueueType = "transient"
+)
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType,
+) (*amqp.Channel, amqp.Queue, error) {
+	channel, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+	defer channel.Close()
+
+	durable := queueType == DurableQueue
+	autoDelete := queueType == TransientQueue
+	exclusive := queueType == TransientQueue
+	queue, err := channel.QueueDeclare(queueName, durable, autoDelete, exclusive, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+	err = channel.QueueBind(queueName, key, exchange, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+	return channel, queue, nil
+}
